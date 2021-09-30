82f4e34ba972be1dff9ee60159b4adbddfa6b172
Zak <zak.solowsky@gmail.com>
2021-09-29 21:57:54 -0400

    added basic math functions (add, subtract, divide, multiply)

---
+++ script.js
@@ -0,0 +1,24 @@
+
+ let operate = function (operator(firstNumber, secondNumber))
+{ 
+
+
+let add = function (firstNumber, secondNumber)
+{ sum = firstNumber + secondNumber;
+return sum;}
+
+let subtract = function (firstNumber, secondNumber)
+{return firstNumber - secondNumber;}
+
+let multiply = function (firstNumber, secondNumber)
+{return firstNumber * secondNumber}
+
+let divide = function (firstNumber, secondNumber)
+{return firstNumber/secondNumber}
+
+}
+
+
+
+ 
+
