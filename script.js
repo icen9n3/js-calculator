78b02aa8ac63d289214fefcd9f606aad3aad2ef1
Zak <zak.solowsky@gmail.com>
2021-10-01 17:53:51 -0400

    changed display text direction to rtl moved display text into its own div and adjusted its location in id=display cleaned up the number button eventlistener with a for loop and already created numberBtn nodelist

--- script.js
+++ script.js
@@ -3,6 +3,42 @@ let firstNumber = 0;
 let operator, plus;
 const numberBtn = document.querySelectorAll(".num")
 
+//adds eventlistener to number buttons
+for(i = 0; i < numberBtn.length; i++)
+{let addNumBtn = numberBtn[i]
+addNumBtn.addEventListener("click", function(){updateDisplay(addNumBtn.innerText)})}
+
+
+// const numberZero = document.querySelector("#num0");
+// numberZero.addEventListener("click", function(){updateDisplay("0")})
+
+// const numberOne = document.querySelector("#num1");
+// numberOne.addEventListener("click", function(){updateDisplay("1")})
+
+// const numberTwo = document.querySelector("#num2");
+// numberTwo.addEventListener("click", function(){updateDisplay("2")})
+
+// const numberThree = document.querySelector("#num3");
+// numberThree.addEventListener("click", function(){updateDisplay("3")})
+
+// const numberFour = document.querySelector("#num4");
+// numberFour.addEventListener("click", function(){updateDisplay("4")})
+
+// const numberFive = document.querySelector("#num5");
+// numberFive.addEventListener("click", function(){updateDisplay("5")})
+
+// const numberSix = document.querySelector("#num6");
+// numberSix.addEventListener("click", function(){updateDisplay("6")})
+
+// const numberSeven = document.querySelector("#num7");
+// numberSeven.addEventListener("click", function(){updateDisplay("7")})
+
+// const numberEight = document.querySelector("#num8");
+// numberEight.addEventListener("click", function(){updateDisplay("8")})
+
+// const numberNine = document.querySelector("#num9");
+// numberNine.addEventListener("click", function(){updateDisplay("9")})
+
 
 
 
@@ -19,7 +55,7 @@ let multiply = function (firstNumber, secondNumber)
 
 let divide = function (firstNumber, secondNumber)
 {y = firstNumber/secondNumber;
-  x = (parseFloat(y).toPrecision(7));
+  x = (parseFloat(y).toPrecision(5));
   decimalAllowed = false;};
 
 
@@ -31,22 +67,35 @@ const decimal = document.querySelector("#decimal");
 decimal.addEventListener("click", function(){decimalCheck(decimalAllowed)})
 
 
+ function checkLength() {
+ if (decimalAllowed == false)   
+ {if(displayField.innerText.length > 10)
+  {displayField.innerText = "error";}
+ else
+  {displayValue = parseFloat(displayField.textContent)}}   
+ 
+ else
+ {if(displayField.innerText.length > 10)
+  {displayField.innerText = "error";
+ resetCheck(1)}
+  else    
+  {displayValue = parseInt(displayField.innerText)};}}
 
 
-function updateDisplay(x){display.textContent =  display.innerText + x; 
-
-   if (decimalAllowed == false)   
-   {if(display.innerText.length > 10)
-    {display.innerText = "error";}
-   else
-    {displayValue = parseFloat(display.textContent)}}   
+function updateDisplay(x){displayField.textContent =  displayField.innerText + x; 
+        checkLength();}
+  //  if (decimalAllowed == false)   
+  //  {if(displayField.innerText.length > 10)
+  //   {displayField.innerText = "error";}
+  //  else
+  //   {displayValue = parseFloat(displayField.textContent)}}   
    
-   else
-   {if(display.innerText.length > 10)
-    {display.innerText = "error";
-  resetCheck(1)}
-    else    
-    {displayValue = parseInt(display.innerText)};}};
+  //  else
+  //  {if(displayField.innerText.length > 10)
+  //   {displayField.innerText = "error";
+  // resetCheck(1)}
+  //   else    
+  //   {displayValue = parseInt(displayField.innerText)};}};
 
 
 function decimalCheck(){
@@ -56,47 +105,9 @@ decimalAllowed = false}}
 
 
 function clearDisplay(){
-display.textContent = "";
+displayField.textContent = "";
 decimalAllowed = true;
 }
-
-   
-
-
-
-
-const numberZero = document.querySelector("#num0");
-numberZero.addEventListener("click", function(){updateDisplay("0")})
-
-const numberOne = document.querySelector("#num1");
-numberOne.addEventListener("click", function(){updateDisplay("1")})
-
-const numberTwo = document.querySelector("#num2");
-numberTwo.addEventListener("click", function(){updateDisplay("2")})
-
-const numberThree = document.querySelector("#num3");
-numberThree.addEventListener("click", function(){updateDisplay("3")})
-
-const numberFour = document.querySelector("#num4");
-numberFour.addEventListener("click", function(){updateDisplay("4")})
-
-const numberFive = document.querySelector("#num5");
-numberFive.addEventListener("click", function(){updateDisplay("5")})
-
-const numberSix = document.querySelector("#num6");
-numberSix.addEventListener("click", function(){updateDisplay("6")})
-
-const numberSeven = document.querySelector("#num7");
-numberSeven.addEventListener("click", function(){updateDisplay("7")})
-
-const numberEight = document.querySelector("#num8");
-numberEight.addEventListener("click", function(){updateDisplay("8")})
-
-const numberNine = document.querySelector("#num9");
-numberNine.addEventListener("click", function(){updateDisplay("9")})
-
-
-
 
 function clear(){
   firstNumber = "";
@@ -108,9 +119,23 @@ function clear(){
  
 }
 
+function backspace() {
+editValue = displayField.innerText;
+displayField.innerText = editValue.substring(0, editValue.length - 1)
+checkLength();}
+
+
+const backspaceBtn = document.querySelector("#backspace");
+  backspaceBtn.addEventListener("click", backspace);
+
+const clearEntry = document.querySelector("#clearEntry");
+clearEntry.addEventListener("click", function(){clearDisplay()})
+
+const clearAll = document.querySelector("#clear");
+clearAll.addEventListener("click", function(){clear()})
+
 const addBtn = document.querySelector("#addition");
 addBtn.addEventListener("click", function(){selectAdd()});
-
 
 const subtractBtn = document.querySelector("#subtract");
 subtractBtn.addEventListener("click", function(){selectSubtract()})
@@ -120,15 +145,6 @@ multiplyBtn.addEventListener("click", function(){selectMultiply()})
 
 const divideBtn = document.querySelector("#divide");
 divideBtn.addEventListener("click", function(){selectDivide()})
-
-
-
-const clearEntry = document.querySelector("#clearEntry");
-clearEntry.addEventListener("click", function(){clearDisplay()})
-
-const clearAll = document.querySelector("#clear");
-clearAll.addEventListener("click", function(){clear()})
-
 
 const equalBtn = document.querySelector("#equals");
   equalBtn.addEventListener("click", operate);
@@ -168,33 +184,27 @@ function selectDivide(){
 
 function operate()
 { 
-
-
   secondNumber = displayValue;
  operator(firstNumber, secondNumber);
  sizeCheck = String(x).length;
  if( sizeCheck > 10)
-  {display.textContent = (parseFloat(x).toPrecision(5));}
+  {displayField.textContent = (parseFloat(x).toPrecision(5));}
   else 
-  {display.textContent = x;}
-  displayValue = x ;
-   
+  {displayField.textContent = x;}
+  displayValue = x ;   
   resetCheck(1);
-  operator = "";
-}
+  operator = "";}
 
 
 
   function reset() {
-    let last = display.innerText 
+    let last = displayField.innerText 
     clear();
     resetCheck(0)
     updateDisplay(lastNum)
     
 
   }
-
-
 
  function resetCheck(value) {
 if (value == 1)
@@ -205,37 +215,15 @@ for(i=0; i < numberBtn.length; i++)
     buttonLoop.addEventListener("click",reset);}
   }
 
-else if  
-
+else if 
 (value == 0)
- {
-for(i=0; i < numberBtn.length; i++)
+ {for(i=0; i < numberBtn.length; i++)
     {buttonLoop = numberBtn[i];
       buttonLoop.removeEventListener("click",reset);}
-    }
-
-}
+    }}
 
 
 
-const backspaceBtn = document.querySelector("#backspace");
-  backspaceBtn.addEventListener("click", backspace);
 
 
-function backspace() {
-editValue = display.innerText;
-display.innerText = editValue.substring(0, editValue.length - 1)
-
-if (decimalAllowed == false)   
-{if(display.innerText.length > 10)
- {display.innerText = "error";}
-else
- {displayValue = parseFloat(display.textContent)}}   
-
-else
-{if(display.innerText.length > 10)
- {display.innerText = "error";
-resetCheck(1)}
- else    
- {displayValue = parseInt(display.innerText)};}};
 
